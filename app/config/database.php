88e89bfc0fa7cd394b6d09cf5e6c2a25881e56ae
lyphuong1990 <kdit.phuonglv@gmail.com>
2016-12-03 23:06:33 +0700

    update daata

--- app/config/database.php
+++ app/config/database.php
@@ -55,7 +55,7 @@ return array(
 		'mysql' => array(
 			'driver'    => 'mysql',
 			'host'      => 'localhost',
-			'database'  => 'inventory_manager',
+			'database'  => 'inventory_management',
 			'username'  => 'root',
 			'password'  => '',
 			'charset'   => 'utf8',
