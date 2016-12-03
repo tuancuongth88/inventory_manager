7193a175d599c6a3b0e1fd441e279e10e571566a
tuancuongth88 <tuancuongth88@gmail.com>
2016-12-03 17:25:45 +0700

    create project

---
+++ app/views/frontend/common/header.blade.php
@@ -0,0 +1,16 @@
+
+<!DOCTYPE html>
+<html lang="en" class=" js csstransforms3d">
+	<head>
+		<meta http-equiv="Content-Type" content="text/html; charset=UTF-8">
+		<meta http-equiv="X-UA-Compatible" content="IE=edge">
+		<meta http-equiv="Content-Type" content="text/html; charset=UTF-8">
+		<title>@yield('title')</title>
+		<!-- <link rel="stylesheet" type="text/css" href="assets/css/bootstrap.min.css"> -->
+		{{HTML::style('assets/css/bootstrap.min.css') }}
+		{{HTML::style('assets/css/bootstrap-theme.min.css') }}
+		{{ HTML::script('http://ajax.googleapis.com/ajax/libs/jquery/1.11.1/jquery.min.js') }}
+		{{ HTML::script('assets/js/bootstrap.min.js') }}
+		{{HTML::style('assets/css/style.css') }}
+	</head>
+	<body>
---
+++ app/views/frontend/layout/default.blade.php
@@ -0,0 +1,11 @@
+@include('fromtend.common.header');
+@include('fromtend.common.menu');
+
+		<div class="content">
+			<div class="main-width">
+				
+				@yield('content')
+			</div>
+		</div>
+@include('fromtend.common.footer');
+
