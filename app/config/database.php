96a5eb68404a1e51a39b598db857bedca3feaa26
tuancuongth88 <tuancuongth88@gmail.com>
2016-12-03 23:02:53 +0700

    update db

--- app/config/database.php
+++ app/config/database.php
@@ -55,7 +55,7 @@ return array(
 		'mysql' => array(
 			'driver'    => 'mysql',
 			'host'      => 'localhost',
-			'database'  => 'apistory',
+			'database'  => 'inventory_manager',
 			'username'  => 'root',
 			'password'  => '',
 			'charset'   => 'utf8',
