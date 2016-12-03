ed3836bea8a2e86780b4c214693a9115622a0ccd
tuancuongth88 <tuancuongth88@gmail.com>
2016-12-03 22:47:17 +0700

    update router

--- app/routes.php
+++ app/routes.php
@@ -10,6 +10,10 @@
 | and give it the Closure to execute when that URI is requested.
 |
 */
+Route::get('/', function()
+{
+    return View::make('admin.layout.login');
+});
 
 Route::group(['prefix' => 'admin'], function () {
 {
